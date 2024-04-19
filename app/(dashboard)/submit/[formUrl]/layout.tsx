c5fed12fdfb4b54e465b68eacb15f91975e266b5
Rahul kumar yadav <77198600+Rahulyadav0296@users.noreply.github.com>
2024-04-20 03:09:54 +0530

    layout.tsx

--- app/(dashboard)/submit/[formUrl]/layout.tsx
+++ app/(dashboard)/submit/[formUrl]/layout.tsx
@@ -1,17 +1,9 @@
-import Logo from "@/components/component/Logo";
-import ThemeSwitcher from "@/components/component/ThemeSwitcher";
 import React, { ReactNode } from "react";
 
-function Layout({ children }: { children: ReactNode }) {
+function layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen h-screen">
-      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
-        <Logo />
-        <ThemeSwitcher />
-      </nav>
-      <main className="flex w-full flex-grow">{children}</main>
-    </div>
+    <div className="flex w-full flex-col flex-grow mx-auto">{children}</div>
   );
 }
 
-export default Layout;
+export default layout;
